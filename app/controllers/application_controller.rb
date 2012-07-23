7e1b6952bfefe97c5213160ffa8e5eaccd4faf9e
Youngv <ootyoungtoo@qq.com>
2012-07-23 21:46:49 +0800

    Task I:Logging In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	before_filter :authorize
 
-  private
+	private
 
-  	def current_cart
-  		Cart.find(session[:cart_id])
-  	rescue ActiveRecord::RecordNotFound
-  		cart = Cart.create
-  		session[:cart_id] = cart.id
-  		cart
-  	end
+	def current_cart
+		Cart.find(session[:cart_id])
+	rescue ActiveRecord::RecordNotFound
+		cart = Cart.create
+		session[:cart_id] = cart.id
+		cart
+	end
+
+	def authorize
+		if User.count.zero?
+    		redirect_to new_user_path, :notice => "Please create a user" 
+    	else
+		unless User.find_by_id(session[:user_id])
+			redirect_to login_url, notice: "Please log in"
+		end
+		end
+	end 
 end
